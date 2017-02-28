74c60204a320258810650d471b1d1e6b5e82d5ab
Tobi Wan <Tobi.Klatt@web.de>
2017-02-28 13:29:08 +0100

    Changed test-function names testFunction --> test_function

--- tests/tests.py
+++ tests/tests.py
@@ -3,14 +3,14 @@ import mp3Tagger
 
 
 class MP3TaggerShould(unittest.TestCase):
-    def testReadDataSucced(self):
+    def test_readDataSucced(self):
         expected = ["Ray Parker, Jr. - Ghostbusters - Ghostbusters - Mr. Metal +.mp3",
                     "Slayer - Raining Blood - Soundtrack To The Apocalypse - Best Of - Mr. Metal +.mp3",
                     "Sodom - Agent Orange - Agent Orange - Mr. Metal +.mp3"]
         actual = mp3Tagger.readData("exampleData")
         self.assertEqual(actual, expected)
 
-    def testReadDataFails(self):
+    def test_readDataFails(self):
         expected = ["Slayer - Raining Blood - Soundtrack To The Apocalypse - Best Of - Mr. Metal +.mp3",
                     "Sodom - Agent Orange - Agent Orange - Mr. Metal +.mp3"]
         actual = mp3Tagger.readData("exampleData")
