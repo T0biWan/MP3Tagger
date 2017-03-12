3bbe0775d01c7ad47e3d958d268b73e6ad610680
Tobi Wan <Tobi.Klatt@web.de>
2017-03-12 16:46:24 +0100

    Implemented functions to read track, year and genre of a song

--- mp3Tagger.py
+++ mp3Tagger.py
@@ -2,7 +2,8 @@ import os
 import mutagen
 
 # Dictionary for ID3v2.4.0 tags
-ID3v24 = {"title": "TIT2", "artist": "TPE1", "album": "TALB"}
+ID3v24 = {"title": "TIT2", "artist": "TPE1", "album": "TALB", "track": "TRCK", "year": "TDRC", "genre": "TCON",
+          "comment": "COMM"}
 
 def readDirectory(path):
     return os.listdir(path)
@@ -17,14 +18,26 @@ def filterMP3(list):
 
 
 def getTitle(song):
-    audio = mutagen.File(song)
-    return audio[ID3v24['title']].text[0]
+    song = mutagen.File(song)
+    return song[ID3v24['title']].text[0]
 
 
 def getArtist(song):
-    audio = mutagen.File(song)
-    return audio[ID3v24['artist']].text[0]
+    song = mutagen.File(song)
+    return song[ID3v24['artist']].text[0]
 
 def getAlbum(song):
-    audio = mutagen.File(song)
-    return audio[ID3v24['album']].text[0]
+    song = mutagen.File(song)
+    return song[ID3v24['album']].text[0]
+
+def getTrack(song):
+    song = mutagen.File(song)
+    return song[ID3v24['track']].text[0]
+
+def getYear(song):
+    song = mutagen.File(song)
+    return song[ID3v24['year']].text[0]
+
+def getGenre(song):
+    song = mutagen.File(song)
+    return song[ID3v24['genre']].text[0]
--- tests/tests.py
+++ tests/tests.py
@@ -9,44 +9,64 @@ class MP3TaggerShould(unittest.TestCase):
                     "Slayer - Raining Blood - Soundtrack To The Apocalypse - Best Of - Mr. Metal +.mp3",
                     "Sodom - Agent Orange - Agent Orange - Mr. Metal +.mp3"]
         actual = mp3Tagger.readDirectory("exampleData")
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_readDirectoryFails(self):
         expected = ["Slayer - Raining Blood - Soundtrack To The Apocalypse - Best Of - Mr. Metal +.mp3",
                     "Sodom - Agent Orange - Agent Orange - Mr. Metal +.mp3"]
         actual = mp3Tagger.readDirectory("exampleData")
-        self.assertNotEqual(actual, expected)
+        self.assertNotEqual(expected, actual)
 
     def test_filterMP3(self):
         expected = ["Ray Parker, Jr. - Ghostbusters - Ghostbusters - Mr. Metal +.mp3",
                     "Slayer - Raining Blood - Soundtrack To The Apocalypse - Best Of - Mr. Metal +.mp3",
                     "Sodom - Agent Orange - Agent Orange - Mr. Metal +.mp3"]
         actual = mp3Tagger.filterMP3(mp3Tagger.readDirectory("exampleData"))
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_getTitle(self):
         expected = "Raining Blood"
         actual = mp3Tagger.readDirectory("exampleData")
         actual = mp3Tagger.filterMP3(actual)
         actual = mp3Tagger.getTitle("exampleData/" + actual[1])
-
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_getArtist(self):
         expected = "Slayer"
         actual = mp3Tagger.readDirectory("exampleData")
         actual = mp3Tagger.filterMP3(actual)
         actual = mp3Tagger.getArtist("exampleData/" + actual[1])
-
-        self.assertEqual(actual, expected)
+        self.assertEqual(expected, actual)
 
     def test_getAlbum(self):
         expected = "Soundtrack To The Apocalypse - Best Of"
         actual = mp3Tagger.readDirectory("exampleData")
         actual = mp3Tagger.filterMP3(actual)
         actual = mp3Tagger.getAlbum("exampleData/" + actual[1])
+        self.assertEqual(expected, actual)
 
-        self.assertEqual(actual, expected)
+    def test_getTrack(self):
+        expected = "1"
+        actual = mp3Tagger.readDirectory("exampleData")
+        actual = mp3Tagger.filterMP3(actual)
+        actual = mp3Tagger.getTrack("exampleData/" + actual[1])
+        self.assertEqual(expected, actual)
+
+    def test_getYear(self):
+        expected = "2003"
+        actual = mp3Tagger.readDirectory("exampleData")
+        actual = mp3Tagger.filterMP3(actual)
+        actual = mp3Tagger.getYear("exampleData/" + actual[1])
+        self.assertEqual(expected, str(actual))
+
+    def test_getGenre(self):
+        expected = "Thrash Metal"
+        actual = mp3Tagger.readDirectory("exampleData")
+        actual = mp3Tagger.filterMP3(actual)
+        actual = mp3Tagger.getGenre("exampleData/" + actual[1])
+        self.assertEqual(expected, actual)
+
+
 
 # Was ist mit Verzeichnissen in verzeichnissen oder Dateien die nicht .mp3 sind?
 # Am besten womöglich rekursiv reingehen
