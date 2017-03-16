9ae7f96491175eb13c546507c1c849088fe6e7aa
Tobi Wan <Tobi.Klatt@web.de>
2017-03-16 19:08:30 +0100

    Added genre to the configuration part

--- tagAudioBooks.py
+++ tagAudioBooks.py
@@ -3,18 +3,20 @@ import mp3Tagger
 
 path = "data/"
 fileExtension = ".mp3"
+genre = "audio"
 comment = "comment"
 
 directory = mp3Tagger.readDirectory(path)
 directory = mp3Tagger.filterMP3(directory)
 
 for audio in directory:
+    mp3Tagger.setGenre(path + audio, genre)
     mp3Tagger.setComment(path + audio, comment)
     mp3Tagger.formatTracknumbersWithLeadingZeros(path + audio, len(str(len(directory))))
     mp3Tagger.setTrackNumberAsTitle(path + audio)
     newFileName = mp3Tagger.buildFileNameFromSomeID3Tags(path + audio) + fileExtension
-    #mp3Tagger.setTrack(path + audio, "")
     mp3Tagger.setFileName(path + audio, path + newFileName)
+    #mp3Tagger.setTrack(path + audio, "")
     print(audio + "\t --> \t" + newFileName)
 
 
